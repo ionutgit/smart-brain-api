7d1f0320bfcadf3ccf05c02d3eebb2bea41b4f59
Ionut Chirita <ionut_chirita@yahoo.com>
2021-10-01 23:22:32 +0300

    user profile route and image route

--- script.js
+++ script.js
@@ -52,6 +52,34 @@ app.post('/register', (req, res) => {
 	res.json(database.users[database.users.length-1])
 })
 
+app.get('/profile/:id', (req, res) => {
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === req.params.id) {
+			found = true;
+			return res.json(user);
+		}
+	})
+	if (!found) {
+		res.status(404).json('no such user');
+	}
+})
+
+app.post('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			user.entries++;
+			return res.json(user.entries);
+		}
+		if (!found) {
+			res.status(400).json('not found');
+		}
+	})
+})
+
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
 })
